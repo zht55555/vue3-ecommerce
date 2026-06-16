aa65bcc93edb2187c5ca04b4b1a2527b468c95b3
张浩天 <zhanghaotian@dip-ai.com>
2026-06-16 17:02:47 +0800

    feat:好物实现

--- src/apis/home.ts
+++ src/apis/home.ts
@@ -1,14 +1,46 @@
 import { type ApiResponse, http } from '@/utils/request'
 
+export interface BannerItem {
+  id: string
+  imgUrl: string
+}
+
+export interface NewItem {
+  id: string
+  name: string
+  price: number
+  picture: string
+}
+
+export interface GoodsItem {
+  id: string
+  name: string
+  price: number
+  picture: string
+  desc: string
+  saleInfo: string
+}
+
+export interface HomeGoodsGroup {
+  id: string
+  name: string
+  price: number
+  picture: string
+  saleInfo: string
+  goods: GoodsItem[]
+}
+
 export const getBanner = (params?: { distributionSite?: string }) => {
   const { distributionSite = '1' } = params || {}
-  return http.get<ApiResponse<{ id: string; imgUrl: string }[]>>('/home/banner', {
+  return http.get<ApiResponse<BannerItem[]>>('/home/banner', {
     params: { distributionSite },
   })
 }
 
 export const getNew = () => {
-  return http.get<ApiResponse<{ id: string; name: string; price: number; picture: string }[]>>(
-    '/home/new',
-  )
+  return http.get<ApiResponse<NewItem[]>>('/home/new')
+}
+
+export function getGoods() {
+  return http.get<ApiResponse<HomeGoodsGroup[]>>('/home/goods')
 }
---
+++ src/views/Home/_components/GoodsItem.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+import { type GoodsItem } from '@/apis/home'
+
+defineProps({
+  good: {
+    type: Object as () => GoodsItem,
+    default: () => ({}),
+  },
+})
+</script>
+<template>
+  <RouterLink :to="`/detail/${good.id}`" class="goods-item">
+    <img v-img-lazy="good.picture" alt="" />
+    <p class="name ellipsis">{{ good.name }}</p>
+    <p class="desc ellipsis">{{ good.desc }}</p>
+    <p class="price">&yen;{{ good.price }}</p>
+  </RouterLink>
+</template>
+<style lang="less">
+.goods-item {
+  display: block;
+  width: 220px;
+  padding: 20px 30px;
+  text-align: center;
+  transition: all 0.5s;
+
+  &:hover {
+    transform: translate3d(0, -3px, 0);
+    box-shadow: 0 3px 8px rgb(0 0 0 / 20%);
+  }
+
+  img {
+    width: 160px;
+    height: 160px;
+  }
+
+  p {
+    padding-top: 10px;
+  }
+
+  .name {
+    font-size: 16px;
+  }
+
+  .desc {
+    color: #999;
+    height: 29px;
+  }
+
+  .price {
+    color: @priceColor;
+    font-size: 20px;
+  }
+}
+</style>
