2c5e1eb281e61428ec73aa56f68998b3ad7b208a
张浩天 <zhanghaotian@dip-ai.com>
2026-06-25 15:32:39 +0800

    fix:路由错误处理

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,7 +5,6 @@ import Category from '@/views/Category/index.vue'
 import SubCategory from '@/views/SubCategory/index.vue'
 import Detail from '@/views/Detail/index.vue'
 import Login from '@/views/Login/index.vue'
-import Practice from '@/views/Practice/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
